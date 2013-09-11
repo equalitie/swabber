b76800981722365ddd2c640752420c38693c0684
Hugh <nosmo@nosmo.me>
2013-09-11 10:07:40 -0400

    Allowing for buffered writes

--- swabber/hostsfile.py
+++ swabber/hostsfile.py
@@ -4,10 +4,16 @@ import os
 
 class HostsFile(object):
 
-    def __init__(self, path):
+    def __init__(self, path, buffer_writes=0):
 
         self.filepath = path
-        self.data = None
+        self.hosts_data = None
+        self.buffer_writes = buffer_writes
+
+        if buffer_writes: 
+            self.buffer = []
+        else:
+            self.buffer = None
 
     def __load_data(self):
         linedata = []
@@ -45,7 +51,7 @@ class HostsFile(object):
         lineentry = self.__render_entry([bantype, ipaddress, comment])
 
         # if entry is the same, do nothing
-        if lineentry in self.hosts_data:
+        if [bantype, ipaddress, comment] in self.hosts_data:
             return self
 
         # if entry exists, update/replace.
@@ -55,11 +61,23 @@ class HostsFile(object):
                 self.hosts_data[index][2] = comment
                 return self
 
+        if self.buffer_writes: 
+            self.buffer.append(lineentry)
+
         # finally, just add it
-        with open(self.filepath, "a") as fileobj:
-            fileobj.write(
-                "%s\n" % lineentry
-            )
+        if not self.buffer_writes:
+            with open(self.filepath, "a") as fileobj:
+                fileobj.write(
+                    "%s\n" % lineentry
+                    )
+        elif self.buffer_writes and len(self.buffer) >= self.buffer_writes:
+            print "Paging out writes"
+            for entry in self.buffer: 
+                with open(self.filepath, "a") as fileobj:
+                    fileobj.write(
+                        "%s\n" % entry
+                        )
+
         self.hosts_data += [bantype, ipaddress, comment]
 
         #TODO: return something better
@@ -135,13 +153,15 @@ class HostsFile(object):
 
 class HostsDeny(HostsFile):
 
-    def __init__(self):
-        HostsFile.__init__(self, "/etc/hosts.deny")
+    def __init__(self, buffer_writes=0):
+        HostsFile.__init__(self, "/etc/hosts.deny", 
+                           buffer_writes=buffer_writes)
 
 class HostsAllow(HostsFile):
 
-    def __init__(self):
-        HostsFile.__init__(self, "/etc/hosts.allow")
+    def __init__(self, buffer_writes=0):
+        HostsFile.__init__(self, "/etc/hosts.allow", 
+                           buffer_writes=buffer_writes)
 
 if __name__ == "__main__":
     # this should be a unit test oh well
