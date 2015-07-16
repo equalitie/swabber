ea3f1f8ab53e8b0a5809ef9aacb215a45d20e6f5
Hugh <nosmo@nosmo.me>
2015-07-16 10:35:14 +0000

    Reorganise, cleanup, make setup.py more pypi friendly

---
+++ src/setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "swabber",
+    version = "1.0.0",
+    author = "Hugh Nowlan",
+    author_email = "nosmo@nosmo.me",
+    description = "Pubsub-based host banning interface",
+    license = "N(C)",
+    keywords = "iptables banning pubsub",
+    url = "http://github.com/equalitie/swabber",
+    packages=['swabber'],
+
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Daemons",
+        "Intended Audience :: System Administrators",
+        "License :: N(C) License",
+        ],
+    scripts = ["swabberd"],
+    data_files=[
+        ('/etc', ["../conf/swabber.yaml"]),
+        ('/etc/init.d/', ["../initscript/swabberd"])
+        ],
+    )
